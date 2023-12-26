9c378c8c4f7e5637091210269a7deb4a772d7b9c
ericlin1234 <yclinmc@gmail.com>
2023-12-26 21:09:22 +0800

    cut down to only 1 lpc, and adding some comments

--- main.m
+++ main.m
@@ -14,6 +14,7 @@ audioInput = audioInput(1:end,1);
 % soundsc(audioInput, fs);
 % pause(length(audioInput)/fs);
 
+% index for the big frame
 hopSize = frameLengthSamples / 2;
 numFrames = floor(length(audioInput) / hopSize) - 1;
 audioInput = audioInput(1:(numFrames*hopSize + hopSize));
@@ -21,12 +22,11 @@ audioInput = audioInput(1:(numFrames*hopSize + hopSize));
 % set output array
 audioOutput = zeros(size(audioInput));
 
-%
+% index for the small frame
 frameLengthSamples2 = 2048;
 hopSize2 = frameLengthSamples2 / 2;
 numFrames2 = floor(frameLengthSamples / hopSize2) - 1;
 
-filteredFrame = zeros([frameLengthSamples,1]);
 tic
 % loop through the frames
 for frameNum = 1:numFrames
@@ -36,22 +36,28 @@ for frameNum = 1:numFrames
     % get the current frame
     frame = audioInput(frameStart:frameEnd);
     
-    % whattttttttt
-    
+    % looping through small frames to get the excitation and OLA
     excitat = zeros([frameLengthSamples, 1]);
+    
+    % for lpc
+    p = 100;                          % lpc coefficient order
+    emphCoef = 0.99;                  % pre-emphasis coefficient
+    A = zeros([numFrames2, p+1]);     % lpc coef matrix
+    
     for frameNum2 = 1:numFrames2
         frameStart2 = (frameNum2-1)*hopSize2+1;
         frameEnd2 = (frameNum2-1)*hopSize2+frameLengthSamples2;
         
+        % get the small frame
         frame2 = frame(frameStart2:frameEnd2);
 
-        p = 100;                         % lpc coefficient order
-        emphCoef = 0.99;                % pre-emphasis coefficient
-        frame2 = filter([1 -emphCoef],1,frame2);
+        % pre-emphasis
+        frame2 = filter([1 -emphCoef],1,frame2);    
 
-        A = lpc(frame2,p);                 % get coefficients
-        frame2 = filter(A,1,frame2);      % get excitation
-
+        A(frameNum2,:) = lpc(frame2,p);           % get lpc coefficients
+        frame2 = filter(A(frameNum2,:),1,frame2); % get excitation
+        
+        % apply window
         frame2 = apply_window(frame2);
 
         % overlap and add
@@ -61,30 +67,27 @@ for frameNum = 1:numFrames
     if frameNum <= numFrames/2
         excitat = shiftPitch(excitat, 3, 'LockPhase',true);
     else
-        excitat = shiftPitch(excitat, -3, 'LockPhase',true);
+        excitat = shiftPitch(excitat, -4, 'LockPhase',true);
     end
-    audiowrite("exc.wav", excitat, fs);
+
+    % looping through small frames to do lpc filtering
     filteredFrame = zeros([frameLengthSamples, 1]);
     for frameNum2 = 1:numFrames2
         frameStart2 = (frameNum2-1)*hopSize2+1;
         frameEnd2 = (frameNum2-1)*hopSize2+frameLengthSamples2;
         
-        frame2 = frame(frameStart2:frameEnd2);
+        % get the small frame of excitation
         frame_ex = excitat(frameStart2:frameEnd2);
+        
+        % re-apply the original lpc coef
+        frame2 = filter(1,A(frameNum2,:),frame_ex);
 
-        p = 100;                         % lpc coefficient order
-        emphCoef = 0.99;                % pre-emphasis coefficient
-        frame2 = filter([1 -emphCoef],1,frame2);
-
-        A = lpc(frame2,p);                 % get coefficients
-
-        frame2 = filter(1,A,frame_ex);
-
+        % de-emphasis
         frame2 = filter(1,[1 -emphCoef],frame2);
-
+        
+        % apply window
         frame2 = apply_window(frame2);
 
-       
         % overlap and add
         filteredFrame(frameStart2:frameEnd2) = filteredFrame(frameStart2:frameEnd2) + frame2;
     end
@@ -99,4 +102,4 @@ toc
 % listen to the audio
 soundsc(audioOutput, fs);
 
-audiowrite("test.wav", audioOutput, fs);
+%audiowrite("test.wav", audioOutput, fs);
